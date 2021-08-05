3f9cf98f7e0e67c345a5487f45b5dd27240f24bc
Haruaki Kobayashi <rigelpain@gmail.com>
2021-08-05 15:54:53 +0900

    [update]  popupIE のヘッダに fontawesome cookie の cdn 追加機能実装

--- PopupIE.js
+++ PopupIE.js
@@ -1,6 +1,15 @@
 $(function() {
 
-  // 仕様ブラウザの取得
+  // headerに font-awesome CDN書き込み
+  var CookieScript = document.createElement('script')
+  CookieScript.setAttribute('src','https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js');
+  document.head.appendChild(CookieScript);
+  var FontawesomeScript = document.createElement('script');  
+  FontawesomeScript.setAttribute('src','https://use.fontawesome.com/releases/v5.15.3/js/all.js');
+  document.head.appendChild(FontawesomeScript);
+
+
+  // 使用ブラウザの取得
   if (window.navigator.userAgent.toLowerCase().indexOf("trident") != -1) {
     console.log("IE");
 
@@ -26,11 +35,9 @@ $(function() {
       outline: 'none',
       padding: '0',
       appearance: 'none',
-    })
-  }).append($('<i>', {class :"fas fa-times-circle"}));
+    })}).append($('<i>', {class :"fas fa-times-circle"}));
     
-    $alertBoxMain = $('<div>', 
-    { 
+    $alertBoxMain = $('<div>', { 
       class:'alert-box-main',
       css:({
         display: 'flex',
@@ -42,8 +49,7 @@ $(function() {
       })
     });
     
-    $alertBox = $('<div>', 
-    { 
+    $alertBox = $('<div>', { 
       class:'alert-box',
       css:({
         // width:'500px',
@@ -59,19 +65,20 @@ $(function() {
     });
 
     $popupArea = $('<div>', { class:'overlay', css:({display:'none', width:'100%', height:'100%', background:'rgba(0,0,0,0.6)', position:'fixed', 'z-index':'1000000'}) });
+    
     // DOMの結合
     $icons.append($cautionIe).append($iconArrow).append($recommendEdge).append($recommendChrome);
     $alertBoxMain.append($btn).append($icons).append($text01).append($text02);
     $alertBox.append($btn).append($alertBoxMain);
     $popupArea.append($alertBox);
 
-    // bodyタグの最後にDOM配置
+    // bodyタグの最初にDOM配置
     $('body').prepend($popupArea);
 
     // 初回閲覧時のみ表示するスクリプト
     $(".overlay").show();
     $.cookie('btnFlg') == 'on' ? $('.overlay').hide() : $('.overlay').show();
-    $('.alert-box button', '.overlay').click(function(){
+    $('.alert-box button').click(function(){
       $('.overlay').fadeOut();
       $.cookie('btnFlg', 'on', { expires: 30,path: '/' }); //cookieの保存
     });
