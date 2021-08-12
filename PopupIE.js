137f73044c681c0353d4c8ce9161e24df64f7262
Haruaki Kobayashi <rigelpain@gmail.com>
2021-08-12 12:04:47 +0900

    [update] アイコンIEのみに変更，テキストの一部を強調

--- PopupIE.js
+++ PopupIE.js
@@ -6,14 +6,15 @@ $(function() {
     var FontawesomeScript = document.createElement('script');  
     FontawesomeScript.setAttribute('src','https://use.fontawesome.com/releases/v5.15.3/js/all.js');
     document.head.appendChild(FontawesomeScript);
-
     // DOMの生成・スタイル指定
-    $text01 = $('<p>', { text:'お使いの Internet Explorer では正しく表示されない場合がございます。', css:({'margin-top': '30px', 'font-weight': 'bold', color:'#888'})});
-    $text02 = $('<p>', { text:'Microsoft Edge もしくは Google Chrome の使用を推奨します。', css:({'margin-top': '10px', 'font-weight': 'bold', color:'#888'})});
+    $text01 = $('<div>', { class: 'text-box', css: ({'margin-top': '30px', display: 'flex', 'justify-content': 'center' }),});
+    $text01_1 = $('<span>', { text:'お使いの Internet Explorer では', css:({'font-weight': 'bold', color:'#888', 'letter-spacing':'.1rem', 'font-family': 'Yu Gothic' })});
+    $text01_2 = $('<span>', { text:'正しく表示されない場合がございます。', css:({'margin-left':'3px', 'font-weight': 'bold', color:'#555', 'letter-spacing':'.1rem', 'font-family': 'Yu Gothic' })});
+    $text02 = $('<p>', { text:'Microsoft Edge もしくは Google Chrome の使用を推奨します。', css:({'margin-top': '10px', 'font-weight': 'bold', color:'#888', 'letter-spacing':'.1rem', 'font-family': 'Yu Gothic' })});
     $cautionIe = $('<i>', { class :"fab fa-internet-explorer", css: ({'font-size': '60px', color: '#BBB'})});
-    $recommendEdge = $('<i>', { class :"fab fa-edge", css: ({'font-size': '60px', color: '#444', 'margin-left':'40px'})});
-    $recommendChrome = $('<i>', { class :"fab fa-chrome", css: ({'font-size': '60px', color: '#444', 'margin-left':'20px'})});
-    $iconArrow = $('<i>', { class :"fas fa-long-arrow-alt-right", css: ({'font-size': '60px', color: '#888', 'margin-left':'40px'})});
+    // $recommendEdge = $('<i>', { class :"fab fa-edge", css: ({'font-size': '60px', color: '#444', 'margin-left':'40px'})});
+    // $recommendChrome = $('<i>', { class :"fab fa-chrome", css: ({'font-size': '60px', color: '#444', 'margin-left':'20px'})});
+    // $iconArrow = $('<i>', { class :"fas fa-long-arrow-alt-right", css: ({'font-size': '60px', color: '#888', 'margin-left':'40px'})});
     $icons = $('<div>', {class: 'icons-box', css: ({display: 'flex', 'justify-content': 'center'}),});
     
     $btn = $('<button>', {css: ({
@@ -61,8 +62,9 @@ $(function() {
     $popupArea = $('<div>', { class:'overlay', css:({display:'none', width:'100%', height:'100%', background:'rgba(0,0,0,0.6)', position:'fixed', 'z-index':'1000000'}) });
     
     // DOMの結合
-    $icons.append($cautionIe).append($iconArrow).append($recommendEdge).append($recommendChrome);
-    $alertBoxMain.append($btn).append($icons).append($text01).append($text02);
+    $icons.append($cautionIe);
+    $text01.append($text01_1).append($text01_2)
+    $alertBoxMain.append($icons).append($text01).append($text02);
     $alertBox.append($btn).append($alertBoxMain);
     $popupArea.append($alertBox);
 
